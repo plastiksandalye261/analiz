b2bbc27bcb73955f31b3cbc242b6ae710ca46ca3
plastiksandalye261 <149300130+plastiksandalye261@users.noreply.github.com>
2023-10-29 13:39:37 +0300

    Untitled5.py

--- Untitled5.py
+++ Untitled5.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyMHhNj15wwW6fSIROAkYHjz",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
